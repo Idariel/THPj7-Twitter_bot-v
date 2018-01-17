b1434f19e4eac32e86bc9b735cb204e672ccb668
admin <isalechat@yahoo.com>
2018-01-17 16:35:34 +0100

    authentification

--- tBot.rb
+++ tBot.rb
@@ -1,1 +1,42 @@
+# ligne très importante qui appelle la gem. Sans elle, le programme ne saura pas appeler Twitter
+require 'twitter'
 
+
+#Necessite de mettre les tokens à la main
+p "consumer_key stp ?"
+twitter_consumer_key = gets.chomp
+p "consumer_secret stp ?"
+twitter_consumer_secret = gets.chomp
+p "consumer_token stp ?"
+twitter_consumer_token = gets.chomp
+p "consumer_token_secret stp ?"
+twitter_consumer_token_secret = gets.chomp
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = twitter_consumer_key
+  config.consumer_secret     = twitter_consumer_secret
+  config.access_token        = twitter_consumer_token
+  config.access_token_secret = twitter_consumer_token_secret
+end
+
+#p client
+
+# ligne qui permet de tweeter
+client.update('Bonjour monde')
+
+
+#Streaming authorization
+client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = "JZHUAH021pcecbxUwBcZV4jpR"
+  config.consumer_secret     = "7j2cygY6Q3WuThXpStUq1plqe6rSwiMegUlsYvb7XUwkWcV4tj"
+  config.access_token        = "4501301836-B1Yw79sgosckPrvH1IQvy71IJB0aCQjg4VBxdMK"
+  config.access_token_secret = "gYlZtGjfGAEpaHYAZnActDEMV8H5yLEeexg8DBqP6Dq2x"
+end
+
+#.take(10) est rajouté pour n'afficher que 10 tweet sinon pb d'arrêt
+topics = ["coffee", "tea"]
+client.filter(track: topics.join(",")) do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
+
+#liker une liste de journalistes
